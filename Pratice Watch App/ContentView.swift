3d63cee9eb79f0863e8a61a43973987be6f4b7ad
Lovepreet kaur <lovepreet8889@gmail.com>
2024-03-25 22:12:29 +0530

    Initial Commit

---
+++ Pratice Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pratice Watch App
+//
+//  Created by Lovepreet kaur on 07/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pratice Watch App/PraticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraticeApp.swift
+//  Pratice Watch App
+//
+//  Created by Lovepreet kaur on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Pratice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
